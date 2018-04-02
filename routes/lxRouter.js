4a97c350dddab0b1ed412cff9c82d69acf8d4fcc
18504750804 <2806481960@qq.com>
2018-04-02 10:57:58 -0400

    render

--- routes/lxRouter.js
+++ routes/lxRouter.js
@@ -6,4 +6,17 @@ router.get('/84', function(req, res, next) {
     res.send("hello lx!");
 });
 
+router.all('/lx',function(req,res,next){
+    console.log("Accessing the lx section");
+    var j={
+        name:"lx",
+        age:16
+    }
+    res.json(j);
+});
+
+router.get('/zx',function(req,res,next){
+    res.render('test',{});
+});
+
 module.exports = router;
